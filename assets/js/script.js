2784242ae4d914b2c9478ad417f0c68bf5580a6f
John Helder <johnhelderjp@gmail.com>
2022-12-03 09:39:11 -0300

    Criando a função para pegar as cores

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,8 @@ document.querySelector('.colorArea .color').forEach(item => {
     item.addEventListener('click', colorClickEvent);
 });
 
- 
+ function colorClickEvent(e){
+    let color = e.target.getAttibute('data-color');
+
+    console.log('cor clicada', color);
+ }
