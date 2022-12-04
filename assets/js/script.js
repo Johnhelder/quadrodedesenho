25d9aeea90db635b659605110b2a841fe11eac06
John Helder <johnhelderjp@gmail.com>
2022-12-04 09:11:42 -0300

    Selecionando a tela

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
+//Initial Data
 let currentColor = 'black';
 
-document.querySelector('.colorArea .color').forEach(item => {
+let screen = document.querySelector('#tela');
+let ctx = screen.getContext('2d');
+
+//Events
+document.querySelectorAll('.colorArea .color').forEach(item => {
     item.addEventListener('click', colorClickEvent);
 });
 
+//Functions
  function colorClickEvent(e){
-    let color = e.target.getAttibute('data-color');
+    let color = e.target.getAttribute('data-color');
     currentColor = color;
 
     document.querySelector('.color.active').classList.remove('active');
     e.target.classList.add('active');
- 
+    
  }
