a8c371a94dbe93a0097ea8831c5cd5b4768adcc5
John Helder <johnhelderjp@gmail.com>
2022-12-08 20:51:02 -0300

    Alterações

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,70 @@
-//Initial Data
+  // Initial data
 let currentColor = 'black';
 let canDraw = false;
 let mouseX = 0;
 let mouseY = 0;
 
 let screen = document.querySelector('#tela');
-let ctx = screen.getContext('2d');
+let ctx =screen.getContext('2d');
 
-//Events
+// Events
 document.querySelectorAll('.colorArea .color').forEach(item => {
     item.addEventListener('click', colorClickEvent);
-});
+})
+
+ 
 screen.addEventListener('mousedown', mouseDownEvent);
 screen.addEventListener('mousemove', mouseMoveEvent);
-screen.addEventListener('mouseUp', mouseUpEvent);
+screen.addEventListener('mouseup', mouseUpEvent);
 document.querySelector('.clear').addEventListener('click', clearScreen);
 
-//Functions
- function colorClickEvent(e){
+// Functions
+function colorClickEvent(e){
     let color = e.target.getAttribute('data-color');
     currentColor = color;
-
+   
     document.querySelector('.color.active').classList.remove('active');
     e.target.classList.add('active');
-    
- }
+}
 
- function mouseDownEvent(e){
+function mouseDownEvent(e){
     canDraw = true;
+    // Saber a posição do mouse:
     mouseX = e.pageX - screen.offsetLeft;
     mouseY = e.pageY - screen.offsetTop;
- }
+}
 
- function mouseMoveEvent(e){
-   if(canDraw){
-      draw(e.pageX, e.pageY);
-   }
- }
+function mouseMoveEvent(e){
+    if(canDraw){
+        draw(e.pageX, e.pageY);
+    }
+}
 
- function mouseUpEvent(e){
+function mouseUpEvent(){
     canDraw = false;
- }
+}
 
- function draw(x, y){
-   let pointX = x - screen.offsetLeft;
-   let pointY = y - screen.offsetTop;
+function draw(x, y){
+    // Posições para o desenho:
+    let pointX = x - screen.offsetLeft;
+    let pointY = y - screen.offsetTop;
 
-   ctx.beginPath();
-   ctx.lineWidth = 5;
-   ctx.lineJoin = "round";
-   ctx.moveTo(mouseX, mouseY);
-   ctx.lineTo(pointX, pointY);
-   ctx.closePath();
-   ctx.strokeStyle = currentColor;
-   ctx.stroke();
+    // desenhar
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.lineJoin = "round";
+    ctx.moveTo(mouseX, mouseY);
+    ctx.lineTo(pointX, pointY);
+    ctx.closePath();
+    ctx.strokeStyle = currentColor;
+    ctx.stroke();
 
-   mouseX = pointX;
-   mouseY = pointY;
- }
+    // salvar a posição anterior:
+    mouseX = pointX;
+    mouseY = pointY;
+}
 
- function clearScreen(){
-   ctx.setTransform(1, 0, 0, 1, 0, 0);
-   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
- }
+function clearScreen(){
+    ctx.setTransform(1,0,0,1,0,0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
